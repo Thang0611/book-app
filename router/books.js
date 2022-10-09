360768848028ad9888de4f92298afc0a07bff53f
Thang Nguyen <nguyenhuuthanga3@gmail.com>
2022-10-09 17:04:38 +0700

    fix

--- router/books.js
+++ router/books.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { getBooks, detailBook, deleteBook, updateBook, addBook } = require('../controller/books.controller');
 const routerBook = express.Router();
 
-routerBook.post('/get-books', getBooks)
+routerBook.get('/get-books', getBooks)
 routerBook.post("/detail-book",detailBook)
 routerBook.post('/delete-book',deleteBook)
 routerBook.post('/update-book',updateBook)
